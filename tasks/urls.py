df9d172b10e78d21be0c590ff70e62d92c30f2b4
psychoAKASH <akash9122000@gmail.com>
2024-11-17 12:33:02 +0530

    class based views replaced by function based views, work in progress

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TaskListView.as_view(), name='task_list'),
-    path('task/create/', views.TaskCreateView.as_view(), name='task_create'),
-    path('task/<int:pk>/edit/', views.TaskUpdateView.as_view(), name='task_edit'),
-    path('task/<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task_delete'),
+    path('', views.task_list, name='task_list'),
+    path('task/create/', views.task_create, name='task_create'),
+    path('task/<int:task_id>/edit/', views.task_edit, name='task_edit'),
+    path('task/<int:task_id>/delete/', views.task_delete, name='task_delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -7,37 +7,53 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
-
-class TaskListView(LoginRequiredMixin, ListView):
-    model = Task
-    template_name = 'tasks/task_list.html'
-
-    def get_queryset(self):
-        return Task.objects.filter(user=self.request.user)
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Task
+from .forms import TaskForm
 
 
-class TaskCreateView(LoginRequiredMixin, CreateView):
-    model = Task
-    fields = ['title', 'description']
-    template_name = 'tasks/task_form.html'
-    success_url = reverse_lazy('task_list')
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)  # Only show tasks of the logged-in user
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
 
-class TaskUpdateView(LoginRequiredMixin, UpdateView):
-    model = Task
-    fields = ['title', 'description']
-    template_name = 'tasks/task_form.html'
-    success_url = reverse_lazy('task_list')
+@login_required
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user  # Associate the task with the logged-in user
+            task.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_form.html', {'form': form})
 
 
-class TaskDeleteView(LoginRequiredMixin, DeleteView):
-    model = Task
-    template_name = 'tasks/task_confirm_delete.html'
-    success_url = reverse_lazy('task_list')
+@login_required
+def task_edit(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)  # Ensure the task belongs to the user
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+
+@login_required
+def task_delete(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)  # Ensure the task belongs to the user
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
 
 
 def register(request):
